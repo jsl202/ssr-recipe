18cc0f2025e7df1c08371fda5255f78a39430cae
jsl202 <jsl202@naver.com>
2023-12-26 11:27:46 +0900

    ssr data loading redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import './App.css';
-import Menu from './components/Menu';
-import { Route, Routes } from 'react-router-dom';
-import RedPage from './pages/RedPage';
-import BluePage from './pages/BluePage';
+import "./App.css";
+import Menu from "./components/Menu";
+import { Route, Routes } from "react-router-dom";
+import RedPage from "./pages/RedPage";
+import BluePage from "./pages/BluePage";
+import UsersPage from "./pages/UsersPage";
 
-
-function App() {
+const App = () => {
   return (
     <div>
       <Menu />
       <hr />
       <Routes>
-        <Route path='/red' element={<RedPage />} />
-        <Route path='/blue' element={<BluePage />} />
+        <Route path="/red" element={<RedPage />} />
+        <Route path="/blue" element={<BluePage />} />
+        <Route path="/users/*" element={<UsersPage />} />
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { applyMiddleware, createStore } from "redux";
+import rootReducer from "./modules";
+import { thunk } from "redux-thunk";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  window.__PRELOADED_STATE__, // 이 값을 초기 상태로 사용함
+  applyMiddleware(thunk)
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
